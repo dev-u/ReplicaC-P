0f27107312e047ce5f4408e7b090f04ac87c2a03
mattosgdanilo@gmail.com <Danilo@Danilos-MacBook-Air.local>
2018-05-03 12:59:11 -0300

    [ADD] Movimento do Player por input do teclado

---
+++ SpaceCP/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Vector2 diretion;
+    private Rigidbody2D player;
+    public float velocity = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        diretion.y = Input.GetAxis("Vertical");
+        diretion.x = Input.GetAxis("Horizontal");
+
+
+        player.AddForce(diretion * velocity);
+	}
+}
